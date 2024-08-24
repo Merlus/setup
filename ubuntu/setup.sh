bb5ab1041b017ef9c0914b4b0e16a6d8fd3ffe30
Michael Roger <michael@merlus.com>
2024-08-24 13:50:58 +0000

    ohmyzsh installed in features

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Install Oh-My-Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 # Install Powerlevel10k theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
