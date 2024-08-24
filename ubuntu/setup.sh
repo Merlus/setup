5097ff183228b6ae6585207609f3635f07291e9d
Michael Roger <michael@merlus.com>
2024-08-24 14:19:04 +0000

    updates

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -13,8 +13,12 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 wget -O $HOME/.zshrc https://raw.githubusercontent.com/Merlus/setup/main/ubuntu/.zshrc &&
 wget -O $HOME/.p10k.zsh https://raw.githubusercontent.com/Merlus/setup/main/ubuntu/.p10k.zsh &&
 
+# reload zsh
+source $HOME/.zshrc &&
+
 # Install Bicep
 az bicep install &&
 
 # Install tfenv
-git clone --depth=1 https://github.com/tfutils/tfenv.git ~/.tfenv
+git clone --depth=1 https://github.com/tfutils/tfenv.git ~/.tfenv &&
+tfenv install latest
