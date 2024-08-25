b1b40b888f4c5e1212c07b913c4d4a887d25870c
Merlus <mail@merlus.com>
2024-08-25 11:09:34 +1000

    Update setup.sh

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -12,6 +12,3 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 # Copy the zshrc file and .p10k.zsh file
 wget -O $HOME/.zshrc https://raw.githubusercontent.com/Merlus/setup/main/ubuntu/.zshrc &&
 wget -O $HOME/.p10k.zsh https://raw.githubusercontent.com/Merlus/setup/main/ubuntu/.p10k.zsh &&
-
-# Install tfenv
-git clone --depth=1 https://github.com/tfutils/tfenv.git ~/.tfenv
